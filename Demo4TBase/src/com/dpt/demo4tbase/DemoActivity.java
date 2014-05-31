4d78a97a7ad044176dd50685a83379875a12a67c
dupengtao <dupengtao88@gmail.com>
2014-06-01 01:58:18 +0800

    load list

--- Demo4TBase/src/com/dpt/demo4tbase/DemoActivity.java
+++ Demo4TBase/src/com/dpt/demo4tbase/DemoActivity.java
@@ -1,13 +1,86 @@
 package com.dpt.demo4tbase;
 
+import java.util.List;
+
+import com.dpt.demo4tbase.adapter.DemoAdapter;
+import com.dpt.demo4tbase.engine.DemoEngine;
+import com.dpt.demo4tbase.engine.to.EntryTo;
+import com.dpt.tbase.app.base.engine.AbUiBaseResultCallBack;
+
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 
 public class DemoActivity extends Activity {
+
+	private ListView mLv;
+	private DemoAdapter mAdapter;
+	private Context mContext;
+	private DemoEngine mDemoEngine;
+	private AbUiBaseResultCallBack<List<EntryTo>> mListCb;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mContext = this;
 		setContentView(R.layout.activity_demo);
+		initView();
+		setEvents();
+	}
+
+	private void initView() {
+		mLv = (ListView) findViewById(R.id.lvDemo);
+	}
+
+	private void setEvents() {
+		initAdapter();
+		initCallBack();
+	}
+
+	private void initCallBack() {
+		mDemoEngine = new DemoEngine(mContext);
+		mListCb = new AbUiBaseResultCallBack<List<EntryTo>>() {
+			@Override
+			public void onSuccessResult(List<EntryTo> datas, int statusCode,
+					String[] otherMsg) {
+				if (datas == null) {
+					Toast.makeText(mContext,
+							"datas==null and statusCode=" + statusCode,
+							Toast.LENGTH_SHORT).show();
+				} else {
+					datas2ui(datas);
+				}
+
+			}
+		};
+	}
+
+	protected void datas2ui(List<EntryTo> datas) {
+		if (datas.size() == 0) {
+			Toast.makeText(mContext, "没有新闻了", Toast.LENGTH_SHORT).show();
+		} else {
+			mAdapter.getList().addAll(datas);
+			if (mLv.getAdapter() == null) {
+				mLv.setAdapter(mAdapter);
+			} else {
+				mAdapter.notifyDataSetChanged();
+			}
+		}
+	}
+
+	private void initAdapter() {
+		mAdapter = new DemoAdapter(mContext);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		loadNews(1, 20);
+	}
+
+	private void loadNews(int pageIndex, int pageSize) {
+		mDemoEngine.loadRecentNews(pageIndex, pageSize, mListCb);
 	}
 }
--- Demo4TBase/src/com/dpt/demo4tbase/adapter/DemoAdapter.java
+++ Demo4TBase/src/com/dpt/demo4tbase/adapter/DemoAdapter.java
@@ -1,15 +1,47 @@
 package com.dpt.demo4tbase.adapter;
 
+import android.content.Context;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.dpt.demo4tbase.R;
 import com.dpt.demo4tbase.engine.to.EntryTo;
 import com.dpt.tbase.app.adapter.AbCustomTBaseAdapter;
 
+
 public class DemoAdapter extends AbCustomTBaseAdapter<EntryTo> {
 
-	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		return null;
-	}
+	private Context mContext;
+
+    public DemoAdapter(Context context) {
+        super();
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = View.inflate(mContext, R.layout.item_news2, null);
+            holder.tvNewsItemTitle = (TextView) convertView.findViewById(R.id.tvNewsItemTitle);
+            holder.tvNewsItemSummary = (TextView) convertView.findViewById(R.id.tvNewsItemSummary);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        EntryTo entry = getList().get(position);
+        holder.tvNewsItemTitle.setText(entry.title);
+        holder.tvNewsItemSummary.setText(Html.fromHtml(entry.summary));
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView tvNewsItemTitle;
+        TextView tvNewsItemSummary;
+    }
 }
