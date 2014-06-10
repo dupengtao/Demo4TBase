0288a3fde975a802ee0b6df93687b0f4dc95de6a
dupengtao <dupengtao>
2014-06-10 21:23:06 +0800

    ignore

--- Demo4TBase/src/com/dpt/demo4tbase/app/DemoApp.java
+++ Demo4TBase/src/com/dpt/demo4tbase/app/DemoApp.java
@@ -1,7 +1,11 @@
 package com.dpt.demo4tbase.app;
 
 import com.dpt.tbase.app.application.TBaseApplication;
-
+/**
+ * text git
+ * @author dupengtao
+ *
+ */
 public class DemoApp extends TBaseApplication {
 
 }
